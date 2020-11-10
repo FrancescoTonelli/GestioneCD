3fbbb91630d506817e48b46c0e1f3b31b9c10626
Framat Carano <wfaffa@outlook.it>
2020-11-10 16:08:18 +0100

    Programma terminato

--- GestioneCD/MainWindow.xaml.cs
+++ GestioneCD/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace GestioneCD
 {
@@ -23,6 +24,102 @@ namespace GestioneCD
         public MainWindow()
         {
             InitializeComponent();
+            AggiornaListaBrani();
+            AggiornaLstBrani();
+            listaCD = new List<CD>();
+        }
+
+        private List<Brano> listaBrani;
+        private List<CD> listaCD;
+
+        private void AggiornaListaBrani()
+        {
+            try
+            {
+                listaBrani = new List<Brano>();
+                using (StreamReader sr = new StreamReader("listaBrani.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null) //i brani sono scritti nel txt come NOME/AUTORE/DURATA
+                    {
+                        string[] line2 = line.Split('/');
+                        Brano p = new Brano(line2[0], line2[1], int.Parse(line2[2]));
+                        listaBrani.Add(p);
+                    }
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Ricontrollare il file txt: " + ex.Message);
+            }
+        }
+
+        private void AggiornaTxt()
+        {
+            using(StreamWriter sw = new StreamWriter("listaBrani.txt"))
+            {
+                for(int i = 0; i < listaBrani.Count; i++)
+                {
+                    sw.WriteLine(listaBrani[i].GetNome() + "/" + listaBrani[i].GetAutore() + "/" + listaBrani[i].GetDurata());
+                }
+            }
+        }
+
+        private void AggiornaLstBrani()
+        {
+            lstBrani.Items.Clear();
+            for (int i = 0; i < listaBrani.Count; i++)
+            {
+                lstBrani.Items.Add(listaBrani[i].ToString());
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Brano p = new Brano(txtNome.Text, txtAutore.Text, int.Parse(txtDurata.Text));
+                listaBrani.Add(p);
+                AggiornaLstBrani();
+                AggiornaTxt();
+                txtNome.Text = "";
+                txtAutore.Text = "";
+                txtDurata.Text = "";
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Reinserire l'input: " + ex.Message);
+            }
+        }
+
+        private void btnCreaCd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Brano> braniCD = new List<Brano>();
+                string[] line = txtListaBrani.Text.Split(',');
+                for(int i = 0; i < line.Length; i++)
+                {
+                    braniCD.Add(listaBrani[int.Parse(line[i]) - 1]);
+                }
+                CD nuovoCd = new CD(txtNome_CD.Text, txtAutore_CD.Text, braniCD);
+                listaCD.Add(nuovoCd);
+                AggiornaLstCd();
+                txtNome_CD.Text = "";
+                txtAutore_CD.Text = "";
+                txtListaBrani.Text = "";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AggiornaLstCd()
+        {
+            lstCD.Items.Clear();
+            for (int i = 0; i < listaCD.Count; i++)
+            {
+                lstCD.Items.Add(listaCD[i].ToString()+" (Durata totale: "+ listaCD[i].Durata() + "sec)");
+            }
         }
     }
 }
